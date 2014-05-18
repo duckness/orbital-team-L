dde50c587b2ef5955a30d03c10f13bb4ad268d76
duckness <duckn355@gmail.com>
2014-05-18 21:23:05 +0800

    simplified test code

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,21 +3,10 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'wow such header'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('wow such header')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Vectorails")
-    end
-
-    it "should have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).to have_title('| Home')
-    end
-
+    it { should have_content('wow such header') }
+    it { should have_title('Vectorails') }
+    it { should have_title('| Home') }
   end
 end
