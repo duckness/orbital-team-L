3356bd3f463f7801ce0d4d211552f5fe8c639e58
duckness <duckn355@gmail.com>
2014-05-26 01:29:51 +0800

    add a proper user model with passwords & test cases

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  #Max lengths are to prevent name from being stupidly long and overflowing on pages, and the rest to prevent attacks on the server with megabyte length strings
+
+  validates :name, presence: true, length: {  maximum: 50  }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: {  maximum: 512  }, uniqueness: {  case_sensitive: false  }
+
+  validates :password, length: {  minimum: 8, maximum: 512  }
+  has_secure_password
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517133831) do
+ActiveRecord::Schema.define(version: 20140525163609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.text     "body"
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
