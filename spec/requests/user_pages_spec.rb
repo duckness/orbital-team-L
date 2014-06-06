7fd8b38c49b77d8d44e1212396de434f271bb035
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2014-06-06 14:29:07 +0700

    Change some layout

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+  require 'spec_helper'
 
 describe "User pages" do
 
@@ -26,17 +26,30 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-    end
-
+    end 
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobarfoo"
         fill_in "Confirmation", with: "foobarfoo"
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
       it "should create a user" do
